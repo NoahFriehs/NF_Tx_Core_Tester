ab0849486d2d31206516a902839fdb1f90848976
mutant_duck <mutantducklp@gmail.com>
2023-11-19 19:11:21 +0100

    Changes for usage in windows

--- main.cpp
+++ main.cpp
@@ -1,33 +1,39 @@
 #include <iostream>
-#include <dlfcn.h> // Dynamic Loading Library
+//#include <dlfcn.h> // Dynamic Loading Library
+#include <windows.h>
+#include <vector>
+#include <fstream>
+#include <sstream>
+
+#include <iostream>
+#include <windows.h>
 #include <vector>
 #include <fstream>
 #include <sstream>
 
 int main() {
-
-    void* libHandle = dlopen("../../NF-Tx-Core/cmake-build-debug/libNF_Tx_Core.so", RTLD_LAZY);
+    HMODULE libHandle = LoadLibraryA("..\\..\\NF_Tx_Core\\cmake-build-debug\\libNF_Tx_Core.dll");
 
     if (!libHandle) {
-        std::cout << "Error loading library: " << dlerror() << std::endl;
+        std::cout << "Error loading library: " << GetLastError() << std::endl;
         return 1;
     }
 
     typedef void (*hello_t)();
-    auto hello = (hello_t) dlsym(libHandle, "hello");
+    auto hello = (hello_t) GetProcAddress(libHandle, "hello");
 
     if (!hello) {
-        std::cout << "Error loading symbol: " << dlerror() << std::endl;
-        dlclose(libHandle);
+        std::cout << "Error loading symbol: " << GetLastError() << std::endl;
+        FreeLibrary(libHandle);
         return 1;
     }
 
-    typedef bool (*initWithData_t)(std::vector<std::string> data, uint mode);
-    auto initWithData = (initWithData_t) dlsym(libHandle, "initWithData");
+    typedef bool (*initWithData_t)(std::vector<std::string> data, int mode);
+    auto initWithData = (initWithData_t) GetProcAddress(libHandle, "initWithData");
 
     if (!initWithData) {
-        std::cout << "Error loading symbol: " << dlerror() << std::endl;
-        dlclose(libHandle);
+        std::cout << "Error loading symbol: " << GetLastError() << std::endl;
+        FreeLibrary(libHandle);
         return 1;
     }
 
@@ -52,11 +58,11 @@ int main() {
     std::cout << "isOk: " << isOk << std::endl;
 
     typedef bool (*init_t)();
-    auto init = (init_t) dlsym(libHandle, "init");
+    auto init = (init_t) GetProcAddress(libHandle, "init");
 
     if (!init) {
-        std::cout << "Error loading symbol: " << dlerror() << std::endl;
-        dlclose(libHandle);
+        std::cout << "Error loading symbol: " << GetLastError() << std::endl;
+        FreeLibrary(libHandle);
         return 1;
     }
 
@@ -64,5 +70,8 @@ int main() {
 
     std::cout << "isOk2: " << isOk2 << std::endl;
 
+    FreeLibrary(libHandle);
+
     return 0;
 }
+
