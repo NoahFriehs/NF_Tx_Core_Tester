14630b137f66a2cc410d2aa7eb8c771721e7e90f
n.friehs <noah.friehs@edu.fh-joanneum.at>
2023-12-10 16:27:14 +0100

    Add StaticPrices and expand currency asset testing

--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@
 #include <vector>
 #include <fstream>
 #include <sstream>
+#include "StaticPrices.h"
 
 int main() {
 
@@ -10,15 +11,6 @@ int main() {
 
     if (!libHandle) {
         std::cout << "Error loading library: " << dlerror() << std::endl;
-        return 1;
-    }
-
-    typedef void (*hello_t)();
-    auto hello = (hello_t) dlsym(libHandle, "hello");
-
-    if (!hello) {
-        std::cout << "Error loading symbol: " << dlerror() << std::endl;
-        dlclose(libHandle);
         return 1;
     }
 
@@ -31,9 +23,7 @@ int main() {
         return 1;
     }
 
-    hello();
-
-    const std::string& filename = "filename.csv";
+    const std::string& filename = "../crypto_sample_transactions.csv";
     std::vector<std::string> data;
 
     std::ifstream file(filename);
@@ -63,5 +53,68 @@ int main() {
 
     std::cout << "isOk2: " << isOk2 << std::endl;
 
+    typedef std::vector<std::string> (*getCurrencies_t)();
+    auto getCurrencies = (getCurrencies_t) dlsym(libHandle, "getCurrencies");
+
+    if (!getCurrencies) {
+        std::cout << "Error loading symbol: " << dlerror() << std::endl;
+        dlclose(libHandle);
+        return 1;
+    }
+
+    auto currencies = getCurrencies();
+
+    for (const auto& item: currencies) {
+        //std::cout << item << std::endl;
+    }
+
+    typedef void (*setPrice_t)(std::vector<double> prices);
+    auto setPrice = (setPrice_t) dlsym(libHandle, "setPrice");
+
+    if (!setPrice) {
+        std::cout << "Error loading symbol: " << dlerror() << std::endl;
+        dlclose(libHandle);
+        return 1;
+    }
+
+    typedef double (*getTotalMoneySpent_t)();
+    auto getTotalMoneySpent = (getTotalMoneySpent_t) dlsym(libHandle, "getTotalMoneySpent");
+
+    if (!getTotalMoneySpent) {
+        std::cout << "Error loading symbol: " << dlerror() << std::endl;
+        dlclose(libHandle);
+        return 1;
+    }
+
+    typedef double (*getTotalValueOfAssets_t)();
+    auto getTotalValueOfAssets = (getTotalValueOfAssets_t) dlsym(libHandle, "getTotalValueOfAssets");
+
+    if (!getTotalValueOfAssets) {
+        std::cout << "Error loading symbol: " << dlerror() << std::endl;
+        dlclose(libHandle);
+        return 1;
+    }
+
+    typedef double (*getTotalBonus_t)();
+    auto getTotalBonus = (getTotalBonus_t) dlsym(libHandle, "getTotalBonus");
+
+    if (!getTotalBonus) {
+        std::cout << "Error loading symbol: " << dlerror() << std::endl;
+        dlclose(libHandle);
+        return 1;
+    }
+
+    std::vector<double> prices;
+    for (const auto &item: currencies) {
+        prices.push_back(getPrice(item));
+    }
+
+    setPrice(prices);
+    std::cout << "Total money spent: " << getTotalMoneySpent() << std::endl;
+    std::cout << "Total value of assets: " << getTotalValueOfAssets() << std::endl;
+    std::cout << "Total bonus: " << getTotalBonus() << std::endl;
+
     return 0;
 }
+
+
